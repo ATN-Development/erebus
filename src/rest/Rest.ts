4a3d726d6671ac9e938128a409598f60e40ae434
D Trombett <maxtromb.dt@gmail.com>
2021-10-19 18:00:42 +0200

    chore: general cleanup (#1)
    * chore: remove useless DOM library and exclude
    The DOM library is only needed in browser projects and we're in Node.js
    We can use the include property instead of an exclude
    
    * chore: update deps and use devDependencies
    Moved to devDependencies all the dependencies that don't need to be installed by the user
    
    * chore: update package-lock
    
    * chore: update ts rules and remove types
    types: We're in a full Typescript project, types are not needed
    allowSyntheticDefaultImports: allows standard default imports
    assumeChangesOnlyAffectDirectDependencies: not needed as we don't use the watch flag
    extendedDiagnostics: not needed
    noErrorTruncation: better to have smaller errors!
    removeComments: comments are not needed in the compiled code
    strict*: already included with the strict option, enabled in the extended configuration
    sourceMap: allows us to easily find where the error is <3
    
    * chore: update gitignore
    the vscode folder should not be included in the repository
    
    * chore: enforce code style with prettier and convert intents to enum
    
    * chore: add dependabot updates

--- src/rest/Rest.ts
+++ src/rest/Rest.ts
@@ -3,25 +3,34 @@ import { Client } from "../Client";
 import Values from "../Values";
 
 export class Rest {
-  _client: Client
-  constructor(client: Client) {
-    this._client = client
-  }
+	_client: Client;
+	constructor(client: Client) {
+		this._client = client;
+	}
 
-  async request(url: string, method: petitio.HTTPMethod, authorization: boolean): Promise<any> {
-    let request = await petitio(`${Values.endpoints.baseURL}${Values.gatewayVersion}/${url}`, method).header("Content-Type", "application/json")
-    if (authorization) {
-      request = request.header("Authorization", this._client.token.startsWith("Bot ") ? this._client.token : `Bot ${this._client.token}`)
-    }
+	async request(url: string, method: petitio.HTTPMethod, authorization: boolean): Promise<any> {
+		let request = await petitio(
+			`${Values.endpoints.baseURL}${Values.gatewayVersion}/${url}`,
+			method
+		).header("Content-Type", "application/json");
+		if (authorization) {
+			request = request.header(
+				"Authorization",
+				this._client.token.startsWith("Bot ") ? this._client.token : `Bot ${this._client.token}`
+			);
+		}
 
-    let response = await request.send()
+		let response = await request.send();
 
-    if (response.headers["x-ratelimit-remaining"] && parseInt(response.headers["x-ratelimit-remaining"]) < 1) {
-      setTimeout(() => {
-        this.request(url, method, authorization)
-      }, response.headers["x-ratelimit-reset"] * 1000)
-    }
+		if (
+			response.headers["x-ratelimit-remaining"] &&
+			parseInt(response.headers["x-ratelimit-remaining"]) < 1
+		) {
+			setTimeout(() => {
+				this.request(url, method, authorization);
+			}, response.headers["x-ratelimit-reset"] * 1000);
+		}
 
-    return response.body.toString()
-  }
+		return response.body.toString();
+	}
 }
--- types/src/rest/Rest.d.ts
+++
@@ -1,7 +0,0 @@
-import petitio from "petitio";
-import { Client } from "../Client";
-export declare class Rest {
-    _client: Client;
-    constructor(client: Client);
-    request(url: string, method: petitio.HTTPMethod, authorization: boolean): Promise<any>;
-}
