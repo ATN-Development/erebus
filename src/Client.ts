1bfd867b00ed45fa44f66493f17eb9f55d5ea39a
DTrombett <maxtromb.dt@gmail.com>
2021-10-19 16:17:33 +0200

    chore: enforce code style with prettier and convert intents to enum

--- src/Client.ts
+++ src/Client.ts
@@ -3,85 +3,81 @@ import WebSocket from "ws";
 import { Rest } from "./rest/Rest";
 
 export interface ClientOptions {
-  token: string;
-  largeThreshold?: number;
-  intents: number | IntentsDefinition[]
+	token: string;
+	largeThreshold?: number;
+	intents: Intents;
 }
 
 export interface HeartbeatInfo {
-  first: boolean;
-  acknowledged: boolean;
+	first: boolean;
+	acknowledged: boolean;
 }
 
-export interface Intents {
-  guilds: 1;
-  guildMembers: 2;
-  guildBans: 4;
-  guildEmojis: 8;
-  guildIntegrations: 16;
-  guildWebhooks: 32;
-  guildInvites: 64;
-  guildVoiceStates: 128;
-  guildPresences: 256;
-  guildMessages: 512;
-  guildMessageReactions: 1024;
-  guildMessageTyping: 2048;
-  directMessages: 4096;
-  directMessageReactions: 8192;
-  directMessageTyping: 16384;
+export enum Intents {
+	guilds = 1,
+	guildMembers = 2,
+	guildBans = 4,
+	guildEmojis = 8,
+	guildIntegrations = 16,
+	guildWebhooks = 32,
+	guildInvites = 64,
+	guildVoiceStates = 128,
+	guildPresences = 256,
+	guildMessages = 512,
+	guildMessageReactions = 1024,
+	guildMessageTyping = 2048,
+	directMessages = 4096,
+	directMessageReactions = 8192,
+	directMessageTyping = 16384,
 }
-
-export type IntentsDefinition = keyof Intents;
 
 export class Client extends EventEmitter {
-  ws?: WebSocket;
-  token: string;
-  largeThreshold: number;
-  heartbeatInfo: HeartbeatInfo;
-  private _rest: Rest;
-  intents: number | IntentsDefinition[]
-  constructor(options: ClientOptions) {
-    super();
-    this.ws = undefined;
-    this.token = options.token.startsWith("Bot ")
-      ? options.token
-      : `Bot ${options.token}`;
-    this.largeThreshold = options.largeThreshold ? options.largeThreshold : 50;
-    this.heartbeatInfo = {
-      first: true,
-      acknowledged: false,
-    };
-    this._rest = new Rest(this);
-    this.intents = options.intents
-  }
+	ws?: WebSocket;
+	token: string;
+	largeThreshold: number;
+	heartbeatInfo: HeartbeatInfo;
+	private _rest: Rest;
+	intents: Intents;
+	constructor(options: ClientOptions) {
+		super();
+		this.ws = undefined;
+		this.token = options.token.startsWith("Bot ") ? options.token : `Bot ${options.token}`;
+		this.largeThreshold = options.largeThreshold ? options.largeThreshold : 50;
+		this.heartbeatInfo = {
+			first: true,
+			acknowledged: false,
+		};
+		this._rest = new Rest(this);
+		this.intents = options.intents;
+	}
 
-  async connect(): Promise<any> {
-    let url = await this.getGateway();
+	async connect(): Promise<any> {
+		let url = await this.getGateway();
 
-    if (!url.endsWith("/")) {
-      url = url + "/";
-    }
+		if (!url.endsWith("/")) {
+			url = url + "/";
+		}
 
-    this.ws = new WebSocket(`${url}?v=9&encoding=json`);
+		this.ws = new WebSocket(`${url}?v=9&encoding=json`);
 
-    this.ws.on("open", () => {});
-  }
+		this.ws.on("open", () => {});
+	}
 
-  async getGateway(): Promise<string> {
-    let response = await this._rest.request("gateway", "GET", false);
-    return response.url;
-  }
+	async getGateway(): Promise<string> {
+		let response = await this._rest.request("gateway", "GET", false);
+		return response.url;
+	}
 
-  private async identify(): Promise<any> {
-    const payload = {
-      token: this.token,
-      properties: {
-        $os: process.platform,
-        $browser: "erebus",
-        $device: "erebus",
-      },
-      large_threshold: this.largeThreshold,
-      intents,
-    };
-  }
+	private async identify(): Promise<any> {
+		const payload = {
+			token: this.token,
+			properties: {
+				$os: process.platform,
+				$browser: "erebus",
+				$device: "erebus",
+			},
+			large_threshold: this.largeThreshold,
+			intents: this.intents,
+		};
+	}
 }
