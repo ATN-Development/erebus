7c64127617b8439f80decf671f3ab5a74bd0380f
Eight∞ <71493060+eightinfinityyt@users.noreply.github.com>
2021-11-16 15:52:16 +0100

    Added documentation for update method in NewsChannel

--- src/structures/NewsChannel.ts
+++ src/structures/NewsChannel.ts
@@ -2,6 +2,11 @@ import type { APITextChannel } from "..";
 import TextChannel from "./TextChannel";
 
 export class NewsChannel extends TextChannel {
+	/**
+	 *
+	 * @param data - The payload of the channel
+	 * @returns {NewsChannel} - The updated class
+	 */
 	update(data: APITextChannel): this {
 		return super.update(data);
 	}
