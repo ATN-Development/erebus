7aa865e77806ef9bdaf961272ac5c1926c77b864
Eight∞ <71493060+eightinfinityyt@users.noreply.github.com>
2021-11-16 17:42:47 +0100

    Added documentation for all classes

--- src/structures/PrivateThreadChannel.ts
+++ src/structures/PrivateThreadChannel.ts
@@ -1,5 +1,8 @@
 import ThreadChannel from "./ThreadChannel";
 
+/**
+ * A guild's private thread channel
+ */
 export class PrivateThreadChannel extends ThreadChannel {}
 
 export default PrivateThreadChannel;
--- src/structures/PublicThreadChannel.ts
+++ src/structures/PublicThreadChannel.ts
@@ -1,5 +1,8 @@
 import ThreadChannel from "./ThreadChannel";
 
+/**
+ * A guild's public thread channel
+ */
 export class PublicThreadChannel extends ThreadChannel {}
 
 export default PublicThreadChannel;
