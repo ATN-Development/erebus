c19c1550de8c6acbbbd56d99d6e66988f291d8c5
Eight∞ <71493060+eightinfinityyt@users.noreply.github.com>
2021-11-16 15:24:39 +0100

    Added update method to Channel class

--- src/structures/Channel.ts
+++ src/structures/Channel.ts
@@ -25,6 +25,17 @@ export class Channel extends Base<APIChannel> {
 		this.id = data.id;
 		this.type = data.type;
 	}
+
+	/**
+	 *
+	 * @param data - The payload of the channel
+	 * @returns {Channel} - The updated class
+	 */
+	update(data: APIChannel): this {
+		if (data.id) this.id = data.id;
+		if (data.type) this.type = data.type;
+		return this;
+	}
 }
 
 export default Channel;
