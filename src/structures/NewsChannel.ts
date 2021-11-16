21a6491d27a4b4934af8678fc48d396bfd75b850
Eight∞ <71493060+eightinfinityyt@users.noreply.github.com>
2021-11-16 16:43:46 +0100

    Fixed News Channel class as the update function was useless to have, just realized mb.

--- src/structures/NewsChannel.ts
+++ src/structures/NewsChannel.ts
@@ -1,17 +1,8 @@
-import type { APITextChannel } from "..";
 import TextChannel from "./TextChannel";
 
 /**
  * A news channel
  */
-export class NewsChannel extends TextChannel {
-	/**
-	 * @param data - The payload of the channel
-	 * @returns {NewsChannel} - The updated class
-	 */
-	update(data: APITextChannel): this {
-		return super.update(data);
-	}
-}
+export class NewsChannel extends TextChannel {}
 
 export default NewsChannel;
